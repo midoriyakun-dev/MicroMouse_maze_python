ff32cd46003a88ad5b809f2598230b33ea87504a
MidoriyaKun <midoriyakun2437@gmail.com>
2025-08-15 22:18:01 +0530

    Added different search strategies , some might be redundant in nature or time and memory complexity. So while implementation take care of that and only implement the unique solutions ...

--- src/search_algo1.py
+++ src/search_algo1.py
@@ -1,1 +1,61 @@
 #An intelligent search algorithm for a random maze the random maze is taken from another python file
+#Pick the search algorithm you want to implement from the list below: then plug into the main run file
+
+
+
+def search_maze_DFS(amze , start):
+    # Initialize the stack with the start position
+    stack = [start]
+    visited = set()
+
+def search_maze_BFS(maze, start):
+    # Initialize the queue with the start position
+    queue = [start]
+    visited = set()
+
+def search_maze_AStar(maze, start):
+    pass
+
+def search_maze_Greedy(maze, start):
+    pass
+
+def search_maze_IDAStar(maze, start):
+    pass
+
+def search_maze_BiDirectional(maze, start, end):
+    pass
+
+def search_maze_UniformCost(maze, start):
+    pass
+
+def search_maze_RecursiveBacktracking(maze, start):
+    pass
+
+def search_maze_HillClimbing(maze, start):
+    pass
+def search_maze_SimulatedAnnealing(maze, start):
+    pass
+def search_maze_IterativeDeepening(maze, start):
+    pass
+
+def search_maze_ConstraintSatisfaction(maze, start):
+    pass
+
+def search_maze_LocalBeam(maze, start):
+    pass
+
+def search_maze_Minimax(maze, start):
+    pass
+    
+def seach_maze_floodFill(maze, start):
+    pass
+
+def search_maze_AlphaBeta(maze, start):
+    pass
+
+def search_maze_IterativeDeepeningAStar(maze, start):
+    pass
+
+def search_maze_RecursiveDescent(maze, start):
+    pass
+

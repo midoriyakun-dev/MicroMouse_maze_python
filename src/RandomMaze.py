45a8c2eccafcfc5bfab4249df81c24a840b9d2c8
MidoriyaKun <midoriyakun2437@gmail.com>
2025-08-15 22:12:27 +0530

    Added RandomMaze generator with a static start point and random end point. Further actions : 2D or 3D renderizing ...

--- src/RandomMaze.py
+++ src/RandomMaze.py
@@ -1,12 +1,12 @@
 import random
 import sys  
 
-def main():
-    # Take a comand line argument for the maze size
+def RandomMaze():
+    # Take a command line argument for the maze size
     # If no argument is provided, default to 10
     size = int(sys.argv[1]) if len(sys.argv) > 1 else 10
     #If the argument is not a valid integer, exit with an error message
-    if size <= 0:
+    if size <= 0:                                                                   #Should I make this <=5? whats a maze size of 1,2,3,4? Not fun to play with
         print("Please provide a positive integer for the maze size.")
         sys.exit(1)
     #If the argument is larger than 100, exit with an error message
@@ -15,8 +15,28 @@ def main():
         sys.exit(1)
     #if the argument is in between 1 and 100, generate a random maze of that size
     maze = generate_maze(size)
+    # Set static start point           //The micromouse will start at (1, 1) so "S" changed to "M"
+    start = (1, 1)
+    maze[start[0]][start[1]] = 'M'
+
+    # Find all possible end points not near the corners (at least 2 cells away from edges)
+    possible_ends = []
+    for i in range(2, size-2):
+        for j in range(2, size-2):
+            if maze[i][j] == ' ':
+                possible_ends.append((i, j))
+    if possible_ends:
+        end = random.choice(possible_ends)                      #Check how random class works or we need to create our own random function
+        maze[end[0]][end[1]] = 'E'
+        
+
+    else:
+        print("No valid end point found away from corners.")
+    
+    print(f"Start: {start}")       # No need to print start point, but keeping it for reference 
+
     #Print the generated maze
-    print_maze(maze)   
+    print_maze(maze)
 
 
 def generate_maze(size):
@@ -29,3 +49,14 @@ def generate_maze(size):
             if j < size - 2 and random.choice([True, False]):
                 maze[i][j + 1] = ' '
     return maze  
+
+def print_maze(maze):
+    for row in maze:
+        print(''.join(row))
+    print() 
+
+if __name__ == "__main__":
+    RandomMaze()
+
+#Can I take this output and use it to represent the maze in a GUI or a wall display? Can i make it 3D rendered?? wall height color and rendering options?
+#What python package to use for 2d or 3d rendering?
